692aa18ab235041c602b8a415de2c35c2155f027
Miroslav Batchkarov <mbatchkarov@gmail.com>
2014-06-06 15:28:18 +0100

    Fixed bug in formula for SI edge breaking probability

--- src/main/java/model/SimulationDynamics.java
+++ src/main/java/model/SimulationDynamics.java
@@ -40,129 +40,129 @@ package model;
  */
 public class SimulationDynamics {
 
-    public enum DynamicsType {
-        SI, SIS, SIR;
-    }
+	public enum DynamicsType {
+		SI, SIS, SIR;
+	}
 
-    private DynamicsType type;
-    //common characteristics of all types of network dynamics
-    private double transmissionRate;
-    private double timeStep;
-    private double recoveryRate;
+	private DynamicsType type;
+	//common characteristics of all types of network dynamics
+	private double transmissionRate;
+	private double timeStep;
+	private double recoveryRate;
 
-    // rates at which edge between S-I, S-S and I-I edges break/appear/rewire
-    private double SIEdgeBreakingRate, SSEdgeBreakingRate, IIEdgeBreakingRate;
-    private double SIEdgeCreationRate, SSEdgeCreationRate, IIEdgeCreationRate;
-    private double SIEdgeRewiringRate, SSEdgeRewiringRate, IIEdgeRewiringRate;
+	// rates at which edge between S-I, S-S and I-I edges break/appear/rewire
+	private double SIEdgeBreakingRate, SSEdgeBreakingRate, IIEdgeBreakingRate;
+	private double SIEdgeCreationRate, SSEdgeCreationRate, IIEdgeCreationRate;
+	private double SIEdgeRewiringRate, SSEdgeRewiringRate, IIEdgeRewiringRate;
 
-    public SimulationDynamics(DynamicsType type, double transmissionRate,
-                              double recoveryRate, double timeStep,
+	public SimulationDynamics(DynamicsType type, double transmissionRate,
+	                          double recoveryRate, double timeStep,
 
-                              double SIEdgeCreationRate,
-                              double SIEdgeBreakingRate,
-                              double SIEdgeRewiringRate,
+	                          double SIEdgeCreationRate,
+	                          double SIEdgeBreakingRate,
+	                          double SIEdgeRewiringRate,
 
-                              double SSEdgeCreationRate,
-                              double SSEdgeBreakingRate,
-                              double SSEdgeRewiringRate,
+	                          double SSEdgeCreationRate,
+	                          double SSEdgeBreakingRate,
+	                          double SSEdgeRewiringRate,
 
-                              double IIEdgeCreationRate,
-                              double IIEdgeBreakingRate,
-                              double IIEdgeRewiringRate
-                             ) {
-        this.type = type;
-        this.transmissionRate = transmissionRate;
-        this.recoveryRate = recoveryRate;
-        this.timeStep = timeStep;
-        this.SIEdgeBreakingRate = SIEdgeBreakingRate;
-        this.SSEdgeBreakingRate = SSEdgeBreakingRate;
-        this.IIEdgeBreakingRate = IIEdgeBreakingRate;
-        this.SIEdgeCreationRate = SIEdgeCreationRate;
-        this.SSEdgeCreationRate = SSEdgeCreationRate;
-        this.IIEdgeCreationRate = IIEdgeCreationRate;
-        this.SIEdgeRewiringRate = SIEdgeRewiringRate;
-        this.SSEdgeRewiringRate = SSEdgeRewiringRate;
-        this.IIEdgeRewiringRate = IIEdgeRewiringRate;
-    }
+	                          double IIEdgeCreationRate,
+	                          double IIEdgeBreakingRate,
+	                          double IIEdgeRewiringRate
+	) {
+		this.type = type;
+		this.transmissionRate = transmissionRate;
+		this.recoveryRate = recoveryRate;
+		this.timeStep = timeStep;
+		this.SIEdgeBreakingRate = SIEdgeBreakingRate;
+		this.SSEdgeBreakingRate = SSEdgeBreakingRate;
+		this.IIEdgeBreakingRate = IIEdgeBreakingRate;
+		this.SIEdgeCreationRate = SIEdgeCreationRate;
+		this.SSEdgeCreationRate = SSEdgeCreationRate;
+		this.IIEdgeCreationRate = IIEdgeCreationRate;
+		this.SIEdgeRewiringRate = SIEdgeRewiringRate;
+		this.SSEdgeRewiringRate = SSEdgeRewiringRate;
+		this.IIEdgeRewiringRate = IIEdgeRewiringRate;
+	}
 
-    public double getRecoveryRate() {
-        if (this.type == DynamicsType.SI)
-            return 0;
-        else return recoveryRate;
-    }
+	public double getRecoveryRate() {
+		if (this.type == DynamicsType.SI)
+			return 0;
+		else return recoveryRate;
+	}
 
-    public double getTransmissionRate() {
-        return transmissionRate;
-    }
+	public double getTransmissionRate() {
+		return transmissionRate;
+	}
 
-    public double getTimeStep() {
-        return timeStep;
-    }
+	public double getTimeStep() {
+		return timeStep;
+	}
 
-    public EpiState getNextState(MyVertex v) {
-        if (this.type == DynamicsType.SI) {
-            return EpiState.INFECTED;
-        } else if (this.type == DynamicsType.SIS) {
-            if (v.isSusceptible()) {
-                return EpiState.INFECTED;
-            } else {
-                return EpiState.SUSCEPTIBLE;
-            }
-        } else {
-            if (v.isSusceptible()) {
-                return EpiState.INFECTED;
-            } else {
-                return EpiState.RESISTANT;
-            }
-        }
-    }
+	public EpiState getNextState(MyVertex v) {
+		if (this.type == DynamicsType.SI) {
+			return EpiState.INFECTED;
+		} else if (this.type == DynamicsType.SIS) {
+			if (v.isSusceptible()) {
+				return EpiState.INFECTED;
+			} else {
+				return EpiState.SUSCEPTIBLE;
+			}
+		} else {
+			if (v.isSusceptible()) {
+				return EpiState.INFECTED;
+			} else {
+				return EpiState.RESISTANT;
+			}
+		}
+	}
 
-    public DynamicsType getType() {
-        return type;
-    }
+	public DynamicsType getType() {
+		return type;
+	}
 
-    public double getRecoveryProb() {
-        return 1d - Math.exp(-1 * (getRecoveryRate() * getTimeStep()));
-    }
+	public double getRecoveryProb() {
+		return 1d - Math.exp(-1 * (getRecoveryRate() * getTimeStep()));
+	}
 
-    public double getInfectionProb() {
-        return 1d - Math.exp((-getTransmissionRate() * getTimeStep()));
-    }
+	public double getInfectionProb() {
+		return 1d - Math.exp(-1d * getTimeStep() * getTransmissionRate());
+	}
 
-    public double getSIEdgeBreakingProb() {
-        return 1 - Math.exp(-1d * SIEdgeBreakingRate);
-    }
+	public double getSIEdgeBreakingProb() {
+		return 1 - Math.exp(-1d * getTimeStep() * SIEdgeBreakingRate);
+	}
 
-    public double getSSEdgeBreakingProb() {
-        return 1 - Math.exp(-1d * getTimeStep() * SSEdgeBreakingRate);
-    }
+	public double getSSEdgeBreakingProb() {
+		return 1 - Math.exp(-1d * getTimeStep() * SSEdgeBreakingRate);
+	}
 
-    public double getIIEdgeBreakingProb() {
-        return 1 - Math.exp(-1d * getTimeStep() * IIEdgeBreakingRate);
-    }
+	public double getIIEdgeBreakingProb() {
+		return 1 - Math.exp(-1d * getTimeStep() * IIEdgeBreakingRate);
+	}
 
-    public double getSIEdgeCreationProb() {
-        return 1 - Math.exp(-1d * getTimeStep() * SIEdgeCreationRate);
-    }
+	public double getSIEdgeCreationProb() {
+		return 1 - Math.exp(-1d * getTimeStep() * SIEdgeCreationRate);
+	}
 
-    public double getSSEdgeCreationProb() {
-        return 1 - Math.exp(-1d * getTimeStep() * SSEdgeCreationRate);
-    }
+	public double getSSEdgeCreationProb() {
+		return 1 - Math.exp(-1d * getTimeStep() * SSEdgeCreationRate);
+	}
 
-    public double getIIEdgeCreationProb() {
-        return 1 - Math.exp(-1d * getTimeStep() * IIEdgeCreationRate);
-    }
+	public double getIIEdgeCreationProb() {
+		return 1 - Math.exp(-1d * getTimeStep() * IIEdgeCreationRate);
+	}
 
-    public double getSIEdgeRewiringProb() {
-        return 1 - Math.exp(-1d * getTimeStep() * SIEdgeRewiringRate);
-    }
+	public double getSIEdgeRewiringProb() {
+		return 1 - Math.exp(-1d * getTimeStep() * SIEdgeRewiringRate);
+	}
 
-    public double getSSEdgeRewiringProb() {
-        return 1 - Math.exp(-1d * getTimeStep() * SSEdgeRewiringRate);
-    }
+	public double getSSEdgeRewiringProb() {
+		return 1 - Math.exp(-1d * getTimeStep() * SSEdgeRewiringRate);
+	}
 
-    public double getIIEdgeRewiringProb() {
-        return 1 - Math.exp(-1d * getTimeStep() * IIEdgeRewiringRate);
-    }
+	public double getIIEdgeRewiringProb() {
+		return 1 - Math.exp(-1d * getTimeStep() * IIEdgeRewiringRate);
+	}
 
 }
